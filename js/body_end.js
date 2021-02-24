df709c04a944b2cf3528ed11600fa59a9a9eb3bf
vovka3003 <vovka3003@ya.ru>
2021-02-24 19:07:54 +0300

    Update body_end.js

--- js/body_end.js
+++ js/body_end.js
@@ -17,14 +17,8 @@ var sections = document.getElementsByTagName('section');
         if (sections[sections.length-1].innerText.indexOf('with Mobirise'))
 		{	sections[sections.length-1].innerHTML = '';	}
 		
+VK.init({apiId: 7427840, onlyWidgets: true});
 
+// тут сообразить div c id vk_comments
 
-<script type="text/javascript">
-  VK.init({apiId: 7427840, onlyWidgets: true});
-</script>
-
-<!-- Put this div tag to the place, where the Comments block will be -->
-<div id="vk_comments"></div>
-<script type="text/javascript">
 VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"});
-</script>
