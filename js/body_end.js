3143134785a9e1d1613095ef80afd86bbebb90ec
vovka3003 <vovka3003@ya.ru>
2021-02-24 19:03:04 +0300

    Update body_end.js

--- js/body_end.js
+++ js/body_end.js
@@ -16,3 +16,15 @@ if (kotletopriemnik != null) {
 var sections = document.getElementsByTagName('section');
         if (sections[sections.length-1].innerText.indexOf('with Mobirise'))
 		{	sections[sections.length-1].innerHTML = '';	}
+		
+
+
+<script type="text/javascript">
+  VK.init({apiId: 7427840, onlyWidgets: true});
+</script>
+
+<!-- Put this div tag to the place, where the Comments block will be -->
+<div id="vk_comments"></div>
+<script type="text/javascript">
+VK.Widgets.Comments("vk_comments", {limit: 10, attach: "*"});
+</script>
